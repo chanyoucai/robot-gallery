636035c7f9318f554ebf7a38f276e3d5266bc64f
yocai <277240773@qq.com>
2022-02-08 21:57:54 +0800

    通过接口获取异步数据及更新试图

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,46 @@ import Robot from './components/Robot';
 import ShoppingCart from './components/ShoppingCart';
 
 
-function App() {
-  return (
+// function App() {
+//   return (
+//     <div className={styles.app}>
+//       <div className={styles.appHeader}>
+//         <img src={logo} alt="logo" className={styles.appLogo} />
+//         <h1>罗伯特机器人</h1>
+//       </div>
+//       <ShoppingCart />
+//       <div className={styles.robotList}>
+//         {robots.map( r => <Robot id={r.id} email={r.email} name={r.name} />)}
+//       </div>
+//     </div>
+//   );
+// }
+
+// 将函数组件修改为类组件
+interface Props {
+
+}
+
+interface State {
+  robotGallery: any[]; // 后端返回的数据类型具有不确定性，因此使用 any 类型
+}
+class App extends React.Component <Props, State> {
+  constructor (props) {
+    super(props)
+    this.state = {
+      robotGallery: [],
+    }
+  }
+
+  // react 声明周期
+  componentDidMount () {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(res => res.json())
+      .then(data => this.setState({ robotGallery: data }))
+  }
+
+  render () {
+   return (
     <div className={styles.app}>
       <div className={styles.appHeader}>
         <img src={logo} alt="logo" className={styles.appLogo} />
@@ -15,10 +53,11 @@ function App() {
       </div>
       <ShoppingCart />
       <div className={styles.robotList}>
-        {robots.map( r => <Robot id={r.id} email={r.email} name={r.name} />)}
+        {this.state.robotGallery.map( r => <Robot id={r.id} email={r.email} name={r.name} />)}
       </div>
     </div>
-  );
+   )
+  }
 }
 
 export default App;

908d9f3d3ff4b179a59dc5af8833e88a72e75f2d
yocai <277240773@qq.com>
2022-02-08 17:07:34 +0800

    使用图片与字体文件

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './assets/images/logo.svg';
 import styles from './App.module.css';
 import robots from './mockdata/robots.json';
 import Robot from './components/Robot';
@@ -8,6 +8,10 @@ import Robot from './components/Robot';
 function App() {
   return (
     <div className={styles.app}>
+      <div className={styles.appHeader}>
+        <img src={logo} alt="logo" className={styles.appLogo} />
+        <h1>罗伯特机器人</h1>
+      </div>
       <div className={styles.robotList}>
         {robots.map( r => <Robot id={r.id} email={r.email} name={r.name} />)}
       </div>
